21278a7e30ba26626136f7572197fee84acb6fae
archibald1418 <liam141810@gmail.com>
2024-09-27 18:34:46 +0100

    initing a fastapi project

---
+++ main.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+'''
+
+- create endpoints
+- add swagger docs (easily done with fastapi
+
+- create database
+- init schema with alembic (initial migration)
+- add ddl code, migrate
+
+- test api with insomnia
+- test api with pytest
+
+- deploy with docker and postgresql
+- run tests (manually or with pytest)
+
+
+'''
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    main()
