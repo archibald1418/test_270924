35185735929f9cd23809a9f3ee86df0b0709ab4f
archibald1418 <liam141810@gmail.com>
2024-09-29 19:20:34 +0100

    integrate postgresql and uuid, added a product into db

--- config.py
+++ config.py
@@ -1,22 +1,35 @@
 from dotenv import load_dotenv
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
-import os 
+import os
+from pprint import pprint
 
-if not load_dotenv(".env"):
+if not load_dotenv(".env", override=True):
     raise Exception("Envs are not loaded, check them please")
 
 DBAPI = os.environ["DBAPI"]
 DB = os.environ["DB"]
+DBURL = None
 
 if DBAPI == 'sqlite':
     DB += '.db'
+    DBURL = f"{DBAPI}:///{DB}"
+    print(DBAPI)
+elif 'pg' in DBAPI:
+    PGUSER = os.environ['PGUSER']
+    PGPASSWORD = os.environ["PGPASSWORD"]
+    PGHOST = os.environ["PGHOST"]
+    PGPORT = os.environ["PGPORT"]
+    
+    DBURL = f'postgresql+{DBAPI}://{PGUSER}@{PGHOST}:{PGPORT}/{DB}'
+
     # engine is great, extra abstraction layer - allows to interchange between sqlite3 and psql
 
-print(f"{DBAPI}:///{DB}")
+print(DBURL)
+# pprint(dict(os.environ.items()))
 
 
-Engine = sqlalchemy.create_engine(f"{DBAPI}:///{DB}", echo=True)
+Engine = sqlalchemy.create_engine(DBURL, echo=True)
 Session = sessionmaker(Engine)
 # session collects transactions and commits them at once
 # also session can do queries (unlike connection)
--- models.py
+++ models.py
@@ -1,11 +1,12 @@
 from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass, Mapped, mapped_column, deferred
+from sqlalchemy.dialects.postgresql import UUID as pg_UUID
 from sqlalchemy.types import DateTime
+import sqlalchemy as sa
 from datetime import datetime
 
-from uuid import uuid4
+from uuid import uuid4, UUID
 
 from _types import OrderStatus
-
 
 class BaseModel(MappedAsDataclass, DeclarativeBase, kw_only=True):
     ...
@@ -14,10 +15,10 @@ class BaseModel(MappedAsDataclass, DeclarativeBase, kw_only=True):
 class Product(BaseModel):
     __tablename__ = "Product"
 
-    id: Mapped[int] = mapped_column(init=False, primary_key=True, default=uuid4().int, repr=False)
+    id: Mapped[UUID] = mapped_column(pg_UUID(as_uuid=True), init=False, primary_key=True, default=uuid4(), repr=False)
     name: Mapped[str] = mapped_column(nullable=False)
     price: Mapped[float] = mapped_column(nullable=False)
-    description: Mapped[str]
+    description: Mapped[str] = mapped_column(nullable=True)
     amount_in_stock: Mapped[int] = mapped_column(default=0)
 
 
